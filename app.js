587d39111ade1900d3d1f3e1042c75c1f9897953
fyd <2221918807@qq.com>
2019-01-25 11:23:32 +0800

    2019年1月25日11:23:29

--- app.js
+++ app.js
@@ -5,6 +5,64 @@ App({
     // var logs = wx.getStorageSync('logs') || []
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
+    console.log("#########登录一次");
+    wx.showLoading({
+      title: '加载中',
+      mask: true,
+    })
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+          wx.clearStorageSync()
+          // 发起网络请求
+          wx.request({
+            url: this.globalData.url + '/api/zyj/onLogin',
+            method: 'POST',
+            data: {
+              code: res.code
+            },
+            success(res) {
+              console.log(res.data);
+              if (res.data.issuccess == 1) {
+
+                wx.setStorageSync('wxcode', res.data.data.code);
+                console.log("当前登录的wxcode" + res.data.data.code);
+                wx.hideLoading();
+               
+              } else {
+                wx.hideLoading();
+                wx.showModal({
+                  title: '错误提示',
+                  content: res.data.msg,
+                })
+              }
+
+            },
+            fail(res) {
+              console.log("失败" + res.data)
+              wx.hideLoading();
+              wx.showModal({
+                title: '错误提示',
+                content: '发生错误，请重试' + res.data,
+              })
+            },
+            complete(res) {
+              console.log("最后" + res)
+            },
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+          wx.hideLoading();
+          wx.showModal({
+            title: '错误提示',
+            content: '发生错误，请重试' + res.errMsg,
+          })
+        }
+      }
+    })
+
 
 
     // 获取用户信息
