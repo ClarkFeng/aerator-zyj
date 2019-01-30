8da5fe3e38c1d9acbe7704f29e8a853e90131aaf
fyd <2221918807@qq.com>
2019-01-30 09:50:51 +0800

    2019年1月30日09:50:48

--- pages/aeratordetail/aeratordetail.js
+++ pages/aeratordetail/aeratordetail.js
@@ -118,7 +118,7 @@ Page({
       mask: true,
     })
     this.downlink("negOpen");
-  }
+  },
 
   downlink: function (commandName){
     var that = this;   // 这个地方非常重要，重置data{}里数据时候setData方法的this应为以及函数的this, 如果在下方的sucess 直接写this就变成了wx.request()的this了
